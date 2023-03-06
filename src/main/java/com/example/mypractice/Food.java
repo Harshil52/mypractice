7eb98bfaf4a6cf5ce892c3b364055a567485c44f
Harshil52 <122923188+Harshil52@users.noreply.github.com>
2023-03-05 21:02:40 -0500

    question 3 complete

--- src/main/java/com/example/mypractice/Food.java
+++ src/main/java/com/example/mypractice/Food.java
@@ -7,4 +7,31 @@ public class Food {
     private double price;
     private int protein;
 
+    public void setName(String name) {
+        if(name.length()<2 || name.trim().isEmpty())
+            this.name = name;
+        else
+            throw new IllegalArgumentException(name + "must have 2 characters and should not be having blank spaces.");
+    }
+
+    public void setCalories(int calories) {
+        if(calories<0 || calories>2000)
+            throw new IllegalArgumentException(calories + "must be in range 0 and 2000");
+        else
+            this.calories = calories;
+    }
+
+    public void setPrice(double price) {
+        if(price<0 || price>150)
+            throw new IllegalArgumentException(price+"must be in range 0 to 150.");
+        else
+            this.price = price;
+    }
+
+    public void setProtein(int protein) {
+        if(protein<0 || protein>100)
+            throw new IllegalArgumentException(protein+"must be in range 0 to 100.");
+        else
+            this.protein = protein;
+    }
 }
