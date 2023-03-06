436e04ca9fbaf832e29804a4b2695f0130c85dda
Harshil52 <122923188+Harshil52@users.noreply.github.com>
2023-03-05 22:05:17 -0500

    question 6 complete

--- src/main/java/com/example/mypractice/Main.java
+++ src/main/java/com/example/mypractice/Main.java
@@ -3,10 +3,18 @@ package com.example.mypractice;
 public class Main {
     public static void main(String[] args) {
 
-        Food food1 = new Food("pizza",50,10.99,10);
-        Food food2 = new Food("burger",60,5.25,20);
+        Food pizza = new Food("pizza",50,10.99,10);
+        Food burger = new Food("burger",60,5.25,20);
 
-        System.out.println(food1.toString());
-        System.out.println(food2.toString());
+        System.out.println(pizza.toString());
+        System.out.println(burger.toString());
+
+        Meal meal = new Meal();
+
+        meal.addFood(pizza);
+        meal.addFood(burger);
+
+        meal.removeFood(burger);
+
     }
 }
--- src/main/java/com/example/mypractice/Meal.java
+++ src/main/java/com/example/mypractice/Meal.java
@@ -5,4 +5,17 @@ import java.util.ArrayList;
 public class Meal {
 
     private ArrayList<Food> meal;
+
+    public Meal() {
+        meal = new ArrayList<>();
+    }
+
+
+
+    public void addFood(Food food) {
+        meal.add(food);
+    }
+    public void removeFood(Food food){
+        meal.remove(food);
+    }
 }
